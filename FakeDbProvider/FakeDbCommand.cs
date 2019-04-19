2c24ab041bf1fcb0df15e189271f15da459f10dc
John Goodwin <john@jjgoodwin.com>
2019-04-19 07:56:27 -0400

    Added LICENSE.txt and updated files which were modified from the upstream source. Original modification was just namespace changes. Later, I'll improve the classes by removing the throw NotImplementedExceptions

--- FakeDbProvider/FakeDbCommand.cs
+++ FakeDbProvider/FakeDbCommand.cs
@@ -1,5 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Note: This file was modified from its original form
 
 using System;
 using System.Data;
